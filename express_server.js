a36f63fab96c7889fef753ecb4e27aa862dcfdc7
Amina Malik <amina.malik@telus.com>
2022-11-28 16:48:36 -0500

    html

--- express_server.js
+++ express_server.js
@@ -11,9 +11,11 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
